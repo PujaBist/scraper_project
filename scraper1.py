ba8fc62efdfa4f27c2b58e99d63c525f53aa3aad
Puja Bist <bistpuja71@gmail.com>
2025-05-08 17:12:59 +0530

    finish project

---
+++ scraper1.py
@@ -0,0 +1,79 @@
+import requests 
+from bs4 import BeautifulSoup 
+import sqlite3
+
+URL="http://books.toscrape.com/"
+def create_table():
+  con=sqlite3.connect("books.sqlite3")
+  cur=con.cursor()
+  cur.execute(
+      """
+    CREATE TABLE  if  not exists books(
+      id integer primary key autoincrement,
+      title text,
+      currency text,
+      price real);
+      """
+    )
+  con.commit()
+  con.close()
+def insert_book(title,currency,price):
+  conn=sqlite3.connect("books.sqlite3")
+  cursor=conn.cursor()
+  cursor.execute("INSERT INTO books(title,currency,price) VALUES (?,?,?)",
+    (title,currency,price),
+    )
+  conn.commit()
+  conn.close()
+
+
+def scrape_books(url) :
+  response=requests.get(url)
+  #print(response) # same way to get requests
+  # another waay to get requests
+  #print(response.status_code)
+  if response.status_code != 200 :
+    return []
+
+  books=[]
+
+    # set encoding explicily to handle special characters correctly
+  response.encoding=response.apparent_encoding
+  #print(response.text)
+  soup = BeautifulSoup(response.text,"html.parser")
+  book_elements=soup.find_all("article",class_="product_pod")
+  #print(book_elements)
+  for book in book_elements :
+    title=book.h3.a['title']
+    price_text=book.find("p", class_="price_color").text
+    currency=price_text[0]
+    price=price_text[1:]
+    books.append(
+      {
+
+      "title":title,
+    "currency":currency,
+    "price":price,}
+    )
+   # print(title,currency,price)
+    #insert_book(title,currency,price)
+  
+
+  print("All books have been scrapped and saved to the database.")
+  return books
+
+
+def save_to_json(books):
+  import json
+  with open("books.json","w",encoding="utf-8") as f :
+    json.dump(books, f,indent=4,ensure_ascii=False)
+
+  print("all books have been saved to json")
+
+
+create_table() 
+
+books=scrape_books(URL)
+
+save_to_json(books)
+
